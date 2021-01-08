9d76b478f2652cb925c6d8994fa16574c328a1f3
Hanbal Ahmed Codes <72215552+hanbalahmed@users.noreply.github.com>
2021-01-08 21:45:12 +0500

    Update Main.js

--- src/Main.js
+++ src/Main.js
@@ -5,13 +5,16 @@ const Main = () => {
     document.title = count;
   });
 
+  const increment = () => setCount(count + 1);
+  const decreament = () => setCount(count - 1);
+
   return (
     <div className="counter">
       <h1 className="counter__number">{count}</h1>
 
       <div className="counterButtons">
-        <button onClick={() => setCount(count + 1)}>Increase Counter</button>
-        <button onClick={() => setCount(count - 1)}>Decrease Counter</button>
+        <button onClick={increment}>Increase Counter</button>
+        <button onClick={decreament}>Decrease Counter</button>
       </div>
     </div>
   );
